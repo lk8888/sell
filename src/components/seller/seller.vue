8ac8bc2f55e2d2b86e1b4a71f9d03332582c5289
liukai <1119240633@qq.com>
2018-02-08 00:11:31 +0800

    seller.vue

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,9 +28,26 @@
 				</div>
 			</div>
 			<split></split>
-			<div class="bulletin">
-				<h1 class="title">公告与活动</h1>
-				<p class="text">{{seller.bulletin}}</p>
+			<div class="info">
+				<div class="bulletin border-1px">
+					<h1 class="title">公告与活动</h1>
+					<p class="text">{{seller.bulletin}}</p>
+				</div>
+				<ul>
+					<li v-for="support in seller.supports" class="support border-1px">
+						<i class="icon" :class="classMap[support.type]"></i>
+						<span class="des">{{support.description}}</span>
+					</li>
+				</ul>
+			</div>
+			<split></split>
+			<div class="scenery">
+				<h1 class="title">商家实景</h1>
+				<div class="pic-wrapper" ref="picWrapper">
+					<ul ref="picContent">
+						<li class="pic-item pic-list-hook" v-for="pic in seller.pics" ref="picItem"><img :src="pic" width="120" height="90" /></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -68,12 +85,35 @@
 				this.scroll = new BScroll(this.$refs.seller, {
 					click: true
 				});
+				this.InitTabScroll();
 			});
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 		},
 		methods: {
 			collect() {
 				this.isActive = !this.isActive;
-			}
+			},
+			InitTabScroll() {
+	          let width = 0;
+	          let picList = this.$refs.picContent.getElementsByClassName('pic-list-hook');
+	          for (let i = 0; i < picList.length; i++) {
+	              width += this.$refs.picItem[i].getBoundingClientRect().width;// getBoundingClientRect() 返回元素的大小及其相对于视口的位置
+	          }
+	          this.$refs.picContent.style.width = width + 'px';
+	          this.$nextTick(() => {
+	              if (!this.picScroll) {
+	                this.picScroll = new BScroll(this.$refs.picWrapper, {
+	                  startX: 0,
+	                  click: true,
+	                  scrollX: true,
+	                  scrollY: false,
+	                  eventPassthrough: 'vertical'
+	                });
+	              } else {
+	                this.picScroll.refresh();
+	              }
+	          });
+	        }
 		}
 	};
 </script>
@@ -148,19 +188,68 @@
 							color: rgb(7, 17, 27)
 							.num
 								font-size: 24px
-			.bulletin
-				padding: 18px 0 16px 0
-				margin: 0 18px
-				border-1px(rgba(7, 17, 27, 0.1))
+			.info
+				padding: 0 18px
+				.bulletin
+					padding: 18px 0 16px 0
+					border-1px(rgba(7, 17, 27, 0.1))
+					.title
+						margin-bottom: 8px
+						line-height: 14px
+						font-size: 14px
+						color: rgb(7, 17, 27)
+					.text
+						padding: 0 12px
+						line-height: 24px
+						font-size: 12px
+						font-weight: 200
+						color: rgb(240, 20, 20)
+				.support
+					padding: 16px 12px
+					border-1px(rgba(7, 17, 27, 0.1))
+					font-size: 0
+					&:last-child
+						border-none()
+					.icon
+						display: inline-block
+						vertical-align: top
+						margin-right: 6px
+						width: 16px
+						height: 16px
+						background-size: 16px 16px
+						background-repeat: no-repeat
+						&.decrease
+							bg-image('decrease_4')
+						&.discount
+							bg-image('discount_4')
+						&.special
+							bg-image('special_4')
+						&.invoice
+							bg-image('invoice_4')
+						&.guarantee
+							bg-image('guarantee_4')
+					.des
+						display: inline-block
+						vertical-align: top
+						line-height: 16px
+						font-size: 12px
+						fonot-weight: 200
+						color: rgb(7, 17, 27)
+			.scenery
+				padding: 18px 0 18px 18px
 				.title
-					margin-bottom: 8px
+					margin-bottom: 12px
 					line-height: 14px
 					font-size: 14px
 					color: rgb(7, 17, 27)
-				.text
-					padding: 0 12px
-					line-height: 24px
-					font-size: 12px
-					font-weight: 200
-					color: rgb(240, 20, 20)
+				.pic-wrapper
+					overflow: hidden
+					white-space: nowrap
+					text-overflow: ellipsis
+					width: 100%
+					ul
+						font-size: 0
+					.pic-item
+						display: inline-block
+						margin-right: 6px
 </style>
